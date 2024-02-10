e510b56c6c517096ce2303c384271cf8c518193a
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-02-09 23:09:13 -0500

    fiex nav bar issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
+    
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar/>
