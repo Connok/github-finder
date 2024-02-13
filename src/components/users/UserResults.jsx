7a783f6ef5cb42107cd2c932e46b370414778447
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-02-13 00:12:33 -0500

    spelling error

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -24,7 +24,7 @@ const UserResults = () => {
 
     if (!loading) {
           return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:gris-cols-3 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
             <h3>{user.login}</h3>
         ))}
